0e47f398850210a399d0c4a26a93520f563835e5
anjalinaragude <96512511+anjalinaragude@users.noreply.github.com>
2026-01-25 21:16:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,17 +16,14 @@ data "aws_subnets" "available_subnets" {
   }
 }
 
-resource "aws_eks_cluster" "project-cluster" {
+resource "aws_eks_cluster" "project_cluster" {
   name     = "project-cluster"
   role_arn = aws_iam_role.example.arn
 
- vpc_config {
-  subnet_ids = data.aws_subnets.available-subnets.ids
-}
+  vpc_config {
+    subnet_ids = data.aws_subnets.available_subnets.ids  # <-- use same name
+  }
 
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
